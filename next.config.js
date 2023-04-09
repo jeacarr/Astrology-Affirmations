0cf89d81d746eb9ae6d176da8b5ce48063c5d931
Jeanine Carryl <jmcarryl@email.meredith.edu>
2023-04-09 01:00:34 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, env: {
+    REACT_APP_API_KEY:"6471948c29mshc84f1b7b6564f85p1021fbjsnd01bbab69cba"
+  },
 }
+
 
 module.exports = nextConfig
